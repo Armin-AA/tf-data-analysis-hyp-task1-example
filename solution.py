d572e782e6f136b65462e839bcac0b35927366ea
Armin-AA <aat6@yandex.ru>
2023-04-12 19:15:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-
-
+import math
+from scipy import stats
 from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 98268891 # Ваш chat ID, не меняйте название переменной
+
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -13,8 +14,11 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    _, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
-    alpha = 0.1
-    effect = (pval < alpha)
+    alpha=0.1
     
-    return effect # Ваш ответ, True или False
+    p_value=proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1] / 2
+    
+    if (p_value < alpha) and (x_success/x_cnt < y_success/y_cnt):
+      return True # Ваш ответ, True или False
+    else: 
+      return False
